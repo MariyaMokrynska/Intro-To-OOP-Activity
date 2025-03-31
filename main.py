45285909cfd1945e31270e8b0bec8caa420f1ddd
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2025-03-30 17:02:32 -0700

    implemented Student class and get_student_with_more_classes() f-n

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from activity.student import Student
 from activity.comparison import get_student_with_more_classes
 
 # first instance
-samara = Student( "Samara", "junior", [ "Pre-Calc", "English III", "World History", "Gym", "Chemistry", "Music Composition" ] )
+samara = Student("Samara", "junior", [
+                 "Pre-Calc", "English III", "World History", "Gym", "Chemistry", "Music Composition"])
 
-samara.add_class("Painting")  # => [ "Pre-Calc", "English III", "World History", "Gym", "Chemistry", "Music Composition", "Painting" ]
+# => [ "Pre-Calc", "English III", "World History", "Gym", "Chemistry", "Music Composition", "Painting" ]
+samara.add_class("Painting")
 
 samara.get_num_classes()  # => 7
 
 samara.summary()  # => "Samara is a junior enrolled in 7 classes"
 
 # second instance
-claire = Student( "Claire", "freshman", [ "Algebra", "Writing", "Contemporary World Issues", "Gym", "Earth Science" ] )
+claire = Student("Claire", "freshman", [
+                 "Algebra", "Writing", "Contemporary World Issues", "Gym", "Earth Science"])
 
-claire.add_class("Painting")  # => [ "Algebra", "Writing", "Contemporary World Issues", "Gym", "Earth Science", "Painting" ]
+# => [ "Algebra", "Writing", "Contemporary World Issues", "Gym", "Earth Science", "Painting" ]
+claire.add_class("Painting")
 
 claire.get_num_classes()  # => 6
 
@@ -21,3 +25,5 @@ claire.summary()  # => "Claire is a freshman enrolled in 6 classes"
 
 # function
 get_student_with_more_classes(claire, samara)  # => samara
+
+print(get_student_with_more_classes(claire, samara))
