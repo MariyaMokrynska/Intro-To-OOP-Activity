e64562403269cd531801de11752298da02a9a407
Marina Adams <bardakover@gmail.com>
2025-03-30 17:10:58 -0700

    Implementation task from READ.md

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,17 @@
-# add your Student class here!
+class Student:
+    def __init__(self, name, class_year, classes):
+        self.name = name
+        self.class_year = class_year
+        self.classes = classes
+    
+    def add_class(self, class_name):
+        self.classes.append(class_name)
+        return self.classes
+
+    def get_num_classes(self):
+        return len(self.classes)
+
+    def summary(self):
+        return f"{self.name} is a {self.class_year} enrolled in {self.get_num_classes()} classes"
+    
+   
